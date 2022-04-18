c8ceb92ecaca655da90e55541ab8ac055ed54e06
Jake Hankins <jakehankins@comcast.net>
2022-04-18 17:11:25 -0400

    commit tree 3392de3b26ccd334ddc2efa0e7d3b6dc9fc418bc

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(drivers, string){
+    const result = drivers.filter(name => name.toLowerCase() === string.toLowerCase())
+    return result;
+}
+function fuzzyMatch(drivers, string){
+    const result = drivers.filter(name => name.startsWith(string))
+    return result
+}
+function matchName(drivers, name){
+    return drivers.filter(function (driver){
+        return driver.name === name
+    })
+}
+ 
